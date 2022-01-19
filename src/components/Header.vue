6ff22fba2ba4d4a8bb4fe662c16f23795388cef4
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2022-01-19 12:30:27 +0100

    create query

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,42 @@
 <template>
     <div>
-        <input type="text" placeholder="Cerca il tuo film">
-        <button>Cerca</button>
+        <input type="text" placeholder="Cerca il tuo film"
+                v-model="searchText"
+                 >
+        <button @click="completeQuery" >Cerca</button>
     </div>
 </template>
 
 <script>
 export default {
     nome:"Header",
+    data () {
+        return {
+            searchText: "",
+            queryStatic:"https://api.themoviedb.org/3/search/movie?api_key=6e3e98384f214afec2e321508faaee73&query=",
+            queryDynamic:"",
+        }
+    },
+
+    methods: {
+        completeQuery() {
+            this.queryDynamic = this.queryStatic + this.searchText;
+            console.log(this.queryDynamic);
+            this.searchText=""
+        } 
+    },
+
+    // mounted () {
+    //     axios
+    //         .get (this.queryDynamic)
+    //         .then((response)=>{
+    //             console.log(response);
+
+    //         }).catch ((error)=>{
+    //             console.log(error);
+    //     })
+    // },
+
 }
 </script>
 
