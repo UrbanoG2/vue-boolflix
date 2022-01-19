5e31ecd414a8ecf9d3b0a96ad8e924151e6c33e2
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2022-01-19 11:41:53 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input type="text" placeholder="Cerca il tuo film">
+        <button>Cerca</button>
+    </div>
+</template>
+
+<script>
+export default {
+    nome:"Header",
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Main
+    </div>
+</template>
+
+<script>
+export default {
+    nome:"Main",
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
